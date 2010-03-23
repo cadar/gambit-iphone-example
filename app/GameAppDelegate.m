078274a2646065d23a6b66783ec2a34d217d4b36
cadar <cadar777@gmail.com>
2010-03-23 13:59:18 +0100

    Added missing files. After renaming.

---
+++ app/GameAppDelegate.m
@@ -0,0 +1,39 @@
+//
+//  tosserAppDelegate.m
+//  tosser
+//
+//  Created by James on 6/2/09.
+//  Copyright Coptix, Inc 2009. All rights reserved.
+//
+
+#import "GameAppDelegate.h"
+#import "EAGLView.h"
+
+@implementation GameAppDelegate
+
+@synthesize window;
+@synthesize glView;
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+
+	glView.animationInterval = 1.0 / 60.0;
+	[glView startAnimation];
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+	glView.animationInterval = 1.0 / 5.0;
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+	glView.animationInterval = 1.0 / 60.0;
+}
+
+- (void)dealloc {
+	[window release];
+	[glView release];
+	[super dealloc];
+}
+
+@end
